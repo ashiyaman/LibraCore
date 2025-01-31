394322e8b12a3a9e0c03d25c547b411c2a8f6c28
ashiyaman <ashiya1005@gmail.com>
2025-01-31 17:21:36 +0530

    feat: Api to add new boo to db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 require('dotenv').config()
+const Books = require('./model/books.models.js')
 
 const {initializeDatabase} = require('./db/db.connect.js')
 const PORT = process.env.PORT 
@@ -11,6 +12,31 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Welcome to Book App Server.')
+})
+
+// Add new book to db
+
+const createBook = async(newBook) => {
+    try{
+        const book = new Books(newBook)
+        const saveNewBooks =  await book.save()
+        return saveNewBooks
+    }
+    catch(error){
+        throw error
+    }
+}
+
+app.post('/books', async (req, res) => {
+    try{
+        const book = await createBook(req.body)
+        if(book){
+            res.status(200).json({message: 'New Book added successfully.', book: book})
+        }
+    }
+    catch(error){
+        res.status(500).json({error: 'Failed to add New Book.'})
+    }
 })
 
 app.listen(PORT, () => {
